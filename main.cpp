8dc50c324d88b80ed4b7dfa271e89b65e9b936fd
piekarsky <ptpiekarski@gmail.com>
2020-09-08 18:20:04 +0200

    convert the image to grayscale

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "lipa.h"
+#include <iostream>
+#include <stack>
+#include <math.h>
+
+
+using namespace std;
+
+
+Image3CH in(1936, 1216);
+Image1CH dst(in.width(), in.height());
+Image1CH tst(2600, 2460);
+Image3CH sym3(SYM_W, SYM_H);
+Image1CH sym1(SYM_W, SYM_H);
+
+
+
+void grayscale(Image3CH & in, Image1CH & out) {		// konwertowanie obrazu do skali szaroœci	
+	for (int i = 0; i < in.width(); i++) {
+		for (int j = 0; j < in.height(); j++) {
+			out(i, j).Intensity() = (in(i, j).Red() + in(i, j).Green() + in(i, j).Blue()) / 3;
+		}
+	}
+}  
+
+int main(){
+
+	// 0 pik   [2]
+	// 1 kier  [7]
+	// 2 trefl [9]
+	// 3 karo  [8]
+
+		
+	in.LoadImage("img\\ideal.jpg", LPL_LOAD_FITTED);		 // wczytanie idealnego zdjêcia
+	in.ShowImage("Original");
+	
+	grayscale(in, dst);
+	
+	return 0;
+}
