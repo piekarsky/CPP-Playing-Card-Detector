2a1c63914722e7e1169c3a50d10d52ebec6bb3c6
piekarsky <ptpiekarski@gmail.com>
2020-09-30 23:48:29 +0200

    Update list

--- list.cpp
+++ list.cpp
@@ -19,27 +19,27 @@ list::~list(){
 
 
 
-void list::push(unsigned int blX, unsigned int blY, unsigned int trX, unsigned int trY, unsigned int inKarta){
+void list::push(unsigned int blX, unsigned int blY, unsigned int trX, unsigned int trY, unsigned int inCard){
 	
-	// push - inserts a new element (bottomleft, topright and inKarta)
+	// push - inserts a new element (bottomleft, topright and inCard)
 	
-	card* karta = new card();		  // initialize a new 'card' object
+	card* cardA = new card();		 	 // initialize a new 'card' object
     if(toP == 0)toP = karta;
-    Id++;							  // inkrement the id
+    Id++;					                   // inkrement the id
     karta->blX = blX;
     karta->blY = blY;
     karta->trX = trX;
     karta->trY = trY;
-    karta->next = boT;				  // karta next = bot bo ustawia wskaźnik na początek
+    karta->next = boT;				  // card next = bot because it sets the pointer to the beginning
     karta->Id   = Id;
-    karta->inKarta  = inKarta;
+    karta->inKarta  = inCard;
     if(karta->next != 0)karta->next->prev = karta;
     karta->prev  = 0;
     karta->karta = 0;
     boT = karta; 
-    acT = karta;					  // indeks do utworzonego obiektu karta
+    acT = karta;					  // index to the created object tab
     for(unsigned int i = 0; i < HIST_VO_; i++){
-        karta->ffT[i] = 0.0;		  // reset this matrix to add items to the histogram
+        karta->ffT[i] = 0.0;				  // reset this matrix to add items to the histogram
     }
 }
 bool list::top(){ 
@@ -94,6 +94,10 @@ void list::get(unsigned int &blX, unsigned int &blY, unsigned int &trX, unsigned
     Id      = acT->Id;
 }
 
-void list::setKarta(int karta){	// set the value to 2, 7, 8, 9
+
+/*
+this function set the value to 2, 7, 8, 9
+*/
+void list::setKarta(int karta){	
     acT->karta = karta;
 }	
